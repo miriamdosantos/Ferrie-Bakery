fa79c22e03d7d4b6d339173f20babc6f8dc011c4
Miriam McCay <mii.santos@yahoo.com.br>
2025-02-15 14:36:40 +0000

    Added checkout views and templates

--- ferrie_bakery/settings.py
+++ ferrie_bakery/settings.py
@@ -48,7 +48,10 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
-    'checkout'
+    'checkout',
+
+     # Other
+    'crispy_forms',
 
 
 ]
@@ -69,7 +72,7 @@ MIDDLEWARE = [
 
 
 ROOT_URLCONF = 'ferrie_bakery.urls'
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -84,8 +87,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                  'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
