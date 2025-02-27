67d5a9d9b54e0da91f20969f38576f28a61f34f7
Miriam McCay <mii.santos@yahoo.com.br>
2025-02-27 14:22:20 +0000

    Added profiles app

--- ferrie_bakery/urls.py
+++ ferrie_bakery/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('products/', include('products.urls')),
+    path('profile/', include('profiles.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
