23c18e73d9c871a94dd7d75d396f1e946e6fdad5
Miriam McCay <mii.santos@yahoo.com.br>
2025-03-04 14:14:57 +0000

    Fix deploy AWS part III

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,12 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
 
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+    default_acl = "public-read"  # Permitir acesso público aos arquivos estáticos
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    file_overwrite = False  # Para evitar sobrescrever arquivos de mídia
