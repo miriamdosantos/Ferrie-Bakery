721c573b21dabad18d5884644e66f33da484bfe0
Miriam McCay <mii.santos@yahoo.com.br>
2025-03-05 22:01:52 +0000

    update urls

--- ferrie_bakery/urls.py
+++ ferrie_bakery/urls.py
@@ -27,6 +27,3 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('profile/', include('profiles.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
